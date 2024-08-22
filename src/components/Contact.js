03632ae14ce751c7e3ede2b8548ecb93c713aef6
hariharan <hariharanofficial1107@gmail.com>
2024-08-22 09:47:27 +0530

    First prototype

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,9 +1,22 @@
 export default function Contact() {
   return (
-    <div>
-      <p>ANy queries and quetions contact us</p>
-      <button>whatsapp chat link</button>
-      <img alt="Facebook,Instagram" />
+    <div className="contact">
+      <h1>
+        <em>Any Question and Queries Related to Our Services</em>
+      </h1>
+      <button className="whatsapp-button">Chat With Us</button>
+      <div className="social-media">
+        <img
+          src="/Assets/facebook.png"
+          alt="facebook"
+          className="facebook"
+        ></img>
+        <img
+          src="/Assets/instagram.png"
+          alt="instagram"
+          className="instagram"
+        ></img>
+      </div>
     </div>
   );
 }
