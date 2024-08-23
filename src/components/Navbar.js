52707726e9c8ca05d223f32b116565df28f8404c
hariharan <hariharanofficial1107@gmail.com>
2024-08-23 08:20:40 +0530

    Hamburger

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,9 @@
 import React, { useEffect, useState } from "react";
 import logo from "./logo.png";
+
 export default function Navbar() {
   const [scrolled, setScrolled] = useState(false);
+  const [menuOpen, setMenuOpen] = useState(false);
 
   useEffect(() => {
     const handleScroll = () => {
@@ -19,17 +21,34 @@ export default function Navbar() {
     };
   }, []);
 
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
   return (
     <nav className={`navbar ${scrolled ? "scrolled" : ""}`}>
       <div className="logo-container">
         <img src={logo} alt="Unique Aerial Vision Logo" className="logo" />
         <h1>Unique Aerial Vision</h1>
       </div>
-      <div className="nav-links">
-        <a href="#hero">Home</a>
-        <a href="#aboutus">About Us</a>
-        <a href="#services">Services</a>
-        <a href="#contact">Contact</a>
+      <div className={`nav-links ${menuOpen ? "active" : ""}`}>
+        <a href="#hero" onClick={() => setMenuOpen(false)}>
+          Home
+        </a>
+        <a href="#aboutus" onClick={() => setMenuOpen(false)}>
+          About Us
+        </a>
+        <a href="#services" onClick={() => setMenuOpen(false)}>
+          Services
+        </a>
+        <a href="#contact" onClick={() => setMenuOpen(false)}>
+          Contact
+        </a>
+      </div>
+      <div className="hamburger" onClick={toggleMenu}>
+        <div></div>
+        <div></div>
+        <div></div>
       </div>
     </nav>
   );
