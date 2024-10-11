28fa6c7fa0cb302972f7d8d13568ec77826fe126
hariharan <hariharanofficial1107@gmail.com>
2024-10-11 22:30:56 +0530

    video added

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,30 +1,12 @@
 import React from "react";
 import Navbar from "./Navbar";
-import "react-responsive-carousel/lib/styles/carousel.min.css"; // Import the carousel styles
-import { Carousel } from "react-responsive-carousel";
 
 export default function Hero() {
   return (
     <div className="hero-container">
-      <Carousel
-        showThumbs={false}
-        autoPlay={true}
-        infiniteLoop={true}
-        showStatus={false}
-        showArrows={true}
-        className="hero-carousel"
-        interval={3000}
-      >
-        <div>
-          <img src="/Assets/image1.webp" alt="Aerial1" loading="lazy" />
-        </div>
-        <div>
-          <img src="Assets/image2.webp" alt="Aerial2" loading="lazy" />
-        </div>
-        <div>
-          <img src="Assets/image3.webp" alt="Aerial3" loading="lazy" />
-        </div>
-      </Carousel>
+      <div>
+        <video src="Assets/video.mp4" autoPlay loop muted />
+      </div>
       <Navbar />
       <div className="hero-content">
         <h2>Capturing the World from Above</h2>
